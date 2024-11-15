5b67d6a1e96730e8fe93a030389924dd656514cd
Piotr Chmielewski <fpt303@gmail.com>
2024-11-15 12:18:39 +0000

    fdsf

---
+++ prog.py
@@ -0,0 +1,22 @@
+if locaction == "in the bedroom":
+    print ("Where in the bedroom should i look")
+    location = input().lower
+if bedroom_locaction == "under the bed":
+    print("Found some shoes but no phone")
+else:
+    print("Found some mess but no phone")
+ elif location == "in the bathroom":
+    print ("Where in the bathroom should i look")
+if bathroom_locaction == "in the bathtub":
+    print("Found the rubber duck but no phone")
+elif location == "in the livingroom":
+    print("Where in the livingroom should i look")
+if living_room_location == "on the table":
+    print("Yes i Found my phone")
+ else:
+    print("Found some stuff but no phone")
+ else:
+    print("FI dont know where that is but i will keep loking for it")
+
+
+
